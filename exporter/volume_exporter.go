655cb57c8473916410cdd8d2b975d051507d9a4f
Love Taneja <taneja.love@gmail.com>
2022-05-12 16:02:33 -0400

    Exporter code

---
+++ exporter/volume_exporter.go
@@ -0,0 +1,41 @@
+package exporter
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type volumeCollector struct {
+	volumeBytesTotal *prometheus.Desc
+	volumeBytesFree  *prometheus.Desc
+	volumeBytesUsed  *prometheus.Desc
+}
+
+func newVolumeCollector() *volumeCollector {
+	namespace := "volume"
+	return &volumeCollector{
+		volumeBytesTotal: prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "bytes_total"), "Total Size", []string{"name", "path"}, nil),
+		volumeBytesFree:  prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "bytes_free"), "Free Size", []string{"name", "path"}, nil),
+		volumeBytesUsed:  prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "bytes_used"), "Used Size", []string{"name", "path"}, nil),
+	}
+}
+
+func (collector *volumeCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- collector.volumeBytesTotal
+	ch <- collector.volumeBytesFree
+	ch <- collector.volumeBytesUsed
+}
+
+func (collector *volumeCollector) Collect(ch chan<- prometheus.Metric) {
+	var metricValue float64
+	if 1 == 1 {
+		metricValue = 1
+	}
+	ch <- prometheus.MustNewConstMetric(collector.volumeBytesTotal, prometheus.GaugeValue, metricValue, "log", "path")
+	ch <- prometheus.MustNewConstMetric(collector.volumeBytesFree, prometheus.GaugeValue, metricValue, "log", "path")
+	ch <- prometheus.MustNewConstMetric(collector.volumeBytesUsed, prometheus.GaugeValue, metricValue, "log", "path")
+}
+
+func Register() {
+	collector := newVolumeCollector()
+	prometheus.MustRegister(collector)
+}
